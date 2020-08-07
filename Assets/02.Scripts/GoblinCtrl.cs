e352d8920f5bb203a39540ce734f0c17dddbb556
Lee JaeHyun <myleje@gmail.com>
2020-08-07 12:18:43 +0900

    .

---
+++ Assets/02.Scripts/GoblinCtrl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinCtrl : MonoBehaviour
+{
+    private Transform tr;       //고블린의 Transform
+    private Transform tigerTr;  //호랑이의 Transform
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
